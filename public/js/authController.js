e7cb43d345b7495703dc9787352060868b325fae
mbreslow56 <mbreslow56@gmail.com>
2017-10-26 01:03:42 +0300

    getting ready to deploy to heroku

--- public/js/authController.js
+++
@@ -1,8 +0,0 @@
-
-app.controller('AuthCtrl', function($scope, $rootScope, $http) {
-  $scope.logout = function() {
-    localStorage.removeItem("user");
-    $rootScope.currentUser = null;
-    delete $http.defaults.headers.common.Authorization;
-  }
-})
---
+++ public/js/controllers/authController.js
@@ -0,0 +1,7 @@
+app.controller('AuthCtrl', function($scope, $rootScope, $http) {
+  $scope.logout = function() {
+    localStorage.removeItem("user");
+    $rootScope.currentUser = null;
+    delete $http.defaults.headers.common.Authorization;
+  }
+})
